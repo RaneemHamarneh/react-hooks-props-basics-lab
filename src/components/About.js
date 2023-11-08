44bac35630551673525219b88a873d7c12cdebd9
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-11-08 10:07:41 +0300

    complete assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const bio = props.bio;
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio?.length > 0 &&
+        <p>{bio}</p>
+      } 
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github ={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
-
+//    if (isLoggedIn) {
+//   button = <LogoutButton onClick={this.handleLogoutClick} />;
+// } else {
+//   button = <LoginButton onClick={this.handleLoginClick} />;
+// }
 export default About;
